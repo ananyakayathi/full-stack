ebf1094709551415f6677b48d2ad958f3852411b
Ananya <ananyakayathi@gmail.com>
2025-03-04 11:11:20 +0530

    updated

---
+++ part06/calc.js
@@ -0,0 +1,9 @@
+export function add(a,b){
+    return a+b
+}
+ export function subtract(a,b){
+    return a-b
+}
+
+//export{add,subtract}
+// we can export both at a time or anyone by giving sperate export to function
